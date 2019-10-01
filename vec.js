9250d503d32f1f32818c970a389cd479d5c9ccd7
Jonas Karg <jonas.karg@hotmail.com>
2019-10-01 18:51:29 +0200

    Moved all the classes into their own files

---
+++ vec.js
@@ -0,0 +1,36 @@
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(other) {
+    return new Vec(
+      this.x + other.x,
+      this.y + other.y
+    );
+  }
+
+  sub(other) {
+    return this.add(other.mul(-1));
+  }
+
+  mul(factor) {
+    return new Vec(
+      this.x * factor,
+      this.y * factor
+    );
+  }
+
+  div(factor) {
+    return this.mul(1 / factor);
+  }
+
+  mag() {
+    // a² + b² = c²
+    return Math.sqrt(
+      Math.pow(this.x, 2) +
+      Math.pow(this.y, 2)
+    );
+  }
+}
